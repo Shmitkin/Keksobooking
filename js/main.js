8657700ab8c199dbe5a8df790e89ca3183aedaee
Shmitkin <mitkinoleg@Gmail.com>
2018-12-13 19:45:39 +0300

    Внес правки из комментариев.

--- js/main.js
+++ js/main.js
@@ -1,289 +1,157 @@
-//Функция случайного числа
-var randomInteger = function (min, max) {
-	var rand = min - 0.5 + Math.random() * (max - min + 1)
-	rand = Math.round(rand);
-	return rand;
-}
+'use strict';
+var CARD_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CARD_CHECK_INS = ['12:00', '13:00', '14:00'];
+var CARD_COUNT = 8;
+var MIN_PRICE = 1000;
+var MAX_PRICE = 1000000;
+var MAX_ROOMS_COUNT = 5;
+var MAX_GUESTS = 20;
+var MIN_X_POSITION = 50;
+var MAX_X_POSITION = 1000;
+var MIN_Y_POSITION = 130;
+var MAX_Y_POSITION = 630;
+var MAP_PIN_WIDTH = 40;
+var MAP_PIN_HEIGHT = 60;
+var CARD_POPUP_NUMBER = 0;
+var CARD_TITLES = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
+var CARD_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var CARD_PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 
-//Функция перемешивания элементов массива 
-var shuffleArray = function (a, b) {
-	return Math.random() - 0.5;
-}
+var getRandomInteger = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
+var getRandomComparison = function () {
+  return Math.random() - 0.5;
+};
 
-// Массив названий карточки
-var cardTitle = [
-	'Большая уютная квартира', 
-	'Маленькая неуютная квартира',
-	'Огромный прекрасный дворец',
-	'Маленький ужасный дворец',
-	'Красивый гостевой домик',
-	'Некрасивый негостеприимный домик',
-	'Уютное бунгало далеко от моря',
-	'Неуютное бунгало по колено в воде'
-]
+var getArrayClone = function (arrayToClone) {
+  var arrayCopy = arrayToClone.slice();
+  arrayCopy.sort(getRandomComparison);
+  return arrayCopy;
+};
 
-//Массив преимуществ
-var cardFeatures = [
-	'wifi',
-	'dishwasher',
-	'parking',
-	'washer',
-	'elevator',
-	'conditioner'
-]
+var getCardFeatures = function (featuresNumber) {
+  var cardFeaturesCopy = getArrayClone(CARD_FEATURES);
+  cardFeaturesCopy.splice(featuresNumber);
+  var featuresList = cardFeaturesCopy.join(',');
+  return featuresList;
+};
 
-//Функция массива преимсуществ
-var cardFeaturesInfo = function (featuresNumber) {
-	cardFeatures.sort(shuffleArray);
-	var featuresList = '';
-	for (var i = 0; i < featuresNumber; i++) {
-		if (i === featuresNumber - 1) {
-			featuresList += cardFeatures[i];
-		} else {
-			featuresList += cardFeatures[i] + ','; 
-		}
-		} 
-		
-	
-	return featuresList;
-}
+var createPhotoNumbers = function (numberOfPhotos) {
+  var photoNumbers = [];
+  for (var i = 1; i < numberOfPhotos + 1; i++) {
+    photoNumbers.push(i);
+  }
+  return photoNumbers;
+};
 
-//Массив для фотографий
-var cardPhotos = [
-	'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-	'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-	'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-]
+var cardTitles = getArrayClone(CARD_TITLES).sort(getRandomComparison);
+var cardTypes = getArrayClone(CARD_TYPES).sort(getRandomComparison);
+var cardPhotos = getArrayClone(CARD_PHOTOS).sort(getRandomComparison);
+var cardCheckIns = getArrayClone(CARD_CHECK_INS).sort(getRandomComparison);
+var cardPhotoNumbers = createPhotoNumbers(CARD_COUNT).sort(getRandomComparison);
 
-//Функция фотографий в случайном порядке 
-var cardPhotosInfo = function () {
-	cardPhotos.sort(shuffleArray);
-	return cardPhotos;
-}
+var getCardsList = function (cardsNumber) {
+  var cardsList = [];
+  for (var i = 0; i < cardsNumber; i++) {
+    var cardLocation = {
+      x: getRandomInteger(MIN_X_POSITION, MAX_X_POSITION),
+      y: getRandomInteger(MIN_Y_POSITION, MAX_Y_POSITION),
+    };
+    var cardOffers = {
+      title: cardTitles[i],
+      address: cardLocation.x + ', ' + cardLocation.y,
+      price: getRandomInteger(MIN_PRICE, MAX_PRICE),
+      type: cardTypes[getRandomInteger(1, CARD_TYPES.length) - 1],
+      rooms: getRandomInteger(1, MAX_ROOMS_COUNT),
+      guests: getRandomInteger(1, MAX_GUESTS),
+      checkin: cardCheckIns[getRandomInteger(1, cardCheckIns.length) - 1],
+      checkout: cardCheckIns[getRandomInteger(1, cardCheckIns.length) - 1],
+      features: getCardFeatures(getRandomInteger(1, CARD_FEATURES.length)),
+      description: '',
+      photos: cardPhotos,
+    };
+    var cardInfo = {
+      avatar: 'img/avatars/user0' + cardPhotoNumbers[i] + '.png',
+      offer: cardOffers,
+      location: cardLocation
+    };
+    cardsList.push(cardInfo);
+  }
+  return cardsList;
+};
 
+var cardsList = getCardsList(CARD_COUNT);
 
-//Массив для номеров фоторгафий аватарок
-var photoNumber = [1,2,3,4,5,6,7,8];
+var removeMapFader = function () {
+  var mapSection = document.querySelector('.map');
+  mapSection.classList.remove('map--faded');
+};
 
-//Массив для типа жилья
-
-var cardType = ['palace', 'flat', 'house', 'bungalo'];
-
-//Массив для времени заезда и выезда
-
-var cardCheckIn = ['12:00', '13:00', '14:00'];
-
-//Перемешиваем все массивы перед началом работы программы
-cardTitle.sort(shuffleArray);
-photoNumber.sort(shuffleArray);
-cardType.sort(shuffleArray);
-cardCheckIn.sort(shuffleArray);
-
-// Создаем массив карточек
-var cardList = [{},{},{},{},{},{},{},{}];
-
-//Создаем обьект карточки
-var cardInfo = {
-	location: {
-		x:'',
-		y:''}
-	}
-
-//Конструктор обьектов карточки
-var Author = function (cardNumber) {
-		this.avatar = ('img/avatars/user0' + photoNumber[cardNumber] + '.png');
-	}
-
-var Offer = function (cardNumber) {
-		this.title = cardTitle[cardNumber],
-		this.address = cardList[cardNumber].location.x + ', ' + cardList[cardNumber].location.y,
-		this.price = randomInteger(1000, 1000000),
-		this.type =cardType[randomInteger(1,4) - 1],
-		this.rooms =randomInteger(1, 5),
-		this.guests =randomInteger(2, 20),
-		this.checkin = cardCheckIn[randomInteger(1,3) - 1],
-		this.checkout =cardCheckIn[randomInteger(1,3) - 1],
-		this.features = cardFeaturesInfo(randomInteger(1,5)),
-		this.description ='',
-		this.photos = cardPhotosInfo() //Почему-то перемешивает 1 раз только
-	}
-var Location = function (cardNumber) {
-		this.x = randomInteger(50, 1000),
-		this.y = randomInteger (130, 630)
-	}
-
-//Функция заполнения карточки и записи в массив
-// Не получается записывать обьекты в пустой массив :(((
-cardRender = function (cardsNumber) {
-for (var i = 0; i < cardsNumber; i++) {
-	cardList[i].location = new Location(i)
-	cardList[i].author = new Author(i);
-	cardList[i].offer = new Offer(i);
-	
-}
-}
-//СОздаем карточки
-var cardsNumber = 8;
-cardRender(cardsNumber);
-
-
-//Удаляем класс map--faded
-
-var mapSection = document.querySelector('.map');
-mapSection.classList.remove('map--faded');
-
-
-//Ищем элемент куда будем вставлять метку
 var mapPins = document.querySelector('.map__pins');
 
-/*
-//Клонирование элемента
-// Ищу элемент который буду копировать
-var mapPin = document.querySelector('#pin')
-	.content
-	.querySelector('.map__pin');
-//Ищу элемент метки куда писать название и олписание
-var mapPinImg = mapPin.getElementsByTagName('img');
-//Добавляю Элемент в разметку и заполняю его
-for (var i = 0; i < cardsNumber; i++) {
-	mapPin.style.left = cardList[i].location.x - 20 + 'px';
-	mapPin.style.top = cardList[i].location.y - 60 + 'px';
-	mapPinImg[0].setAttribute('src', cardList[i].author.avatar);
-	mapPinImg[0].setAttribute('alt', cardList[i].offer.title)
-	var similarMapPin = mapPin.cloneNode(true);
-	mapPins.appendChild(similarMapPin);
-	
-};
-*/
-
-//Создаем элемент через СОЗДАНИЕ
 var pinsCreation = function (pinsNumber) {
-var pinFragment = document.createDocumentFragment();
-// Создаем элемент, заполняем, записываем в фрагмент
-for (var i = 0; i < pinsNumber; i++) {
-	var pinElement = document.createElement('button');
-	pinElement.className = 'map__pin';
-	pinElement.style.left = cardList[i].location.x - 20 + 'px';
-	pinElement.style.top = cardList[i].location.y - 60 + 'px';
-	pinElement.innerHTML = '<img src="' 
-		+ cardList[i].author.avatar 
-		+ '"' 
-		+ 'width="40" height="40" draggable="false" alt="' 
-		+ cardList[i].offer.title 
-		+ '">';
-	pinFragment.appendChild(pinElement);
-}
-//Вставляем наш фрагмент
-mapPins.appendChild(pinFragment);
-}
-/*
-//Присваиваем русское значение типу жилья
-cardTypeToRus = function () {
-	if (cardList[0].offer.type = 'palace') {'Дворец'} else
-		if (cardList[0].offer.type = 'flat') {'Квартира'} else
-			if (cardList[0].offer.type = 'house') {'Дом'} else
-				if (cardList[0].offer.type = 'bungalo') {'Бунгало'}
-}
-console.log(cardList[0].offer.type);
-*/
+  var pinFragment = document.createDocumentFragment();
+  for (var i = 0; i < pinsNumber; i++) {
+    var pinElement = document.createElement('button');
+    pinElement.className = 'map__pin';
+    pinElement.style.left = cardsList[i].location.x - (MAP_PIN_WIDTH / 2) + 'px';
+    pinElement.style.top = cardsList[i].location.y - MAP_PIN_HEIGHT + 'px';
+    pinElement.innerHTML = '<img src="'
+      + cardsList[i].avatar
+      + '"'
+      + 'width="40" height="40" draggable="false" alt="'
+      + cardsList[i].offer.title
+      + '">';
+    pinFragment.appendChild(pinElement);
+  }
+  mapPins.appendChild(pinFragment);
+};
 
-//Создаем элемент карточки на основе наших данных
-var renderCardElement = function (j) {
-	var cardElement = document.createElement('article');
-		cardElement.className = 'map__card popup';
-
-	var cardElementImg = document.createElement('img');
-		cardElementImg.className = 'popup__avatar';
-		cardElementImg.src = cardList[0].author.avatar;
-		cardElementImg.width = '70';
-		cardElementImg.height = '70';
-		cardElementImg.alt = 'Аватар пользователя';
-
-	var cardElementButton = document.createElement('button');
-		cardElementButton.type = 'button';
-		cardElementButton.className = 'popup__close';
-		cardElementButton.innerHTML = 'Закрыть';
-
-	var cardElementTitle = document.createElement('h3');
-		cardElementTitle.className = 'popup__title';
-		cardElementTitle.innerHTML =  cardList[0].offer.title;
-
-	var cardElementAddress = document.createElement('p');
-		cardElementAddress.className = 'popup__text popup__text--address';
-		cardElementAddress.innerHTML = cardList[0].offer.address;
-
-	var cardElementPrice = document.createElement('p');
-		cardElementPrice.className = 'popup__text popup__text--price';
-		cardElementPrice.innerHTML = cardList[0].offer.price + '&#x20bd;<span>/ночь</span>';
-
-	var cardElementType = document.createElement('h4');
-		cardElementType.className = 'popup__type';
-		cardElementType.innerHTML = cardList[0].offer.type;  // - перевести
-
-	var cardElementCapacity = document.createElement('p');
-		cardElementCapacity.className = 'popup__text popup__text--capacity';
-		cardElementCapacity.innerHTML = cardList[0].offer.rooms 
-			+ ' комнаты для ' 
-			+ cardList[0].offer.guests 
-			+ ' гостей';
-
-	var cardElementTime = document.createElement('p');
-		cardElementTime.className = 'popup__text popup__text--time';
-		cardElementTime.innerHTML = 'Заезд после ' 
-			+ cardList[0].offer.checkin 
-			+ ', выезд до ' 
-			+ cardList[0].offer.checkout;
-
-	var cardElementFeatures = document.createElement('ul');
-		cardElementFeatures.className = 'popup__features';
-		// разбиваем строку в массив
-		cardFeaturesArray = cardList[0].offer.features.split(',');
-		for (var i = 0; i < cardFeaturesArray.length; i++) {
-			var cardElementFeature = document.createElement('li');
-			cardElementFeature.className = 'popup__feature popup__feature--' + cardFeaturesArray[i];
-			cardElementFeatures.appendChild(cardElementFeature);
-		}
-
-	var cardElementDescription = document.createElement('p');
-		cardElementDescription.className = 'popup__description';
-		cardElementDescription.innerHTML = cardList[0].offer.description;
-
-	var cardElementPhotos = document.createElement('div');
-		for (var i = 0; i < cardList[0].offer.photos.length; i++) {
-			var cardElementPhoto = document.createElement('img');
-			cardElementPhoto.className = 'popup__photo';
-			cardElementPhoto.width = '45';
-			cardElementPhoto.height = '40';
-			cardElementPhoto.alt = 'Фотография жилья';
-			cardElementPhoto.src = cardList[0].offer.photos[i];
-			cardElementPhotos.appendChild(cardElementPhoto);
-		}
-
-	//Сборка карточки
-	cardElement.appendChild(cardElementImg);
-	cardElement.appendChild(cardElementButton);
-	cardElement.appendChild(cardElementTitle);
-	cardElement.appendChild(cardElementAddress);
-	cardElement.appendChild(cardElementPrice);
-	cardElement.appendChild(cardElementType);
-	cardElement.appendChild(cardElementCapacity);
-	cardElement.appendChild(cardElementTime);
-	cardElement.appendChild(cardElementFeatures);
-	cardElement.appendChild(cardElementDescription);
-	cardElement.appendChild(cardElementPhotos);
-
-
-	//вставляем в ДОМ карточку
-	document.querySelector('.map').appendChild(cardElement);
-}
-//console.log(cardList[0].offer.photos[1]);
-
-
-
-//Вызываем отрисовку страницы
-pinsCreation(8);
-renderCardElement();
-
-
-
+var createPopUpInfo = function (cardNumber) {
+  var card = document.querySelector('#card').content.cloneNode(true);
+  card.querySelector('.popup__avatar').src = cardsList[cardNumber].avatar;
+  card.querySelector('.popup__title').innerHTML = cardsList[cardNumber].offer.title;
+  card.querySelector('.popup__text--address').innerHTML = cardsList[cardNumber].offer.address;
+  card.querySelector('.popup__text--price').innerHTML = cardsList[cardNumber].offer.price + '&#x20bd;<span>/ночь</span>';
+  card.querySelector('.popup__type').innerHTML = cardsList[cardNumber].offer.type;
+  card.querySelector('.popup__text--capacity').innerHTML = cardsList[cardNumber].offer.rooms
+  + ' комнаты для '
+  + cardsList[cardNumber].offer.guests
+  + ' гостей';
+  card.querySelector('.popup__text--time').innerHTML = 'Заезд после '
+  + cardsList[cardNumber].offer.checkin
+  + ', выезд до '
+  + cardsList[cardNumber].offer.checkout;
+  card.querySelector('.popup__photo').src = cardsList[cardNumber].offer.photos[0];
+  for (var i = 1; i < cardsList[cardNumber].offer.photos.length; i++) {
+    var anotherImg = card.querySelector('.popup__photo').cloneNode(true);
+    anotherImg.src = cardsList[cardNumber].offer.photos[i];
+    card.querySelector('.popup__photos').appendChild(anotherImg);
+  }
+  document.querySelector('.map').appendChild(card);
+};
+removeMapFader();
+pinsCreation(CARD_COUNT);
+createPopUpInfo(CARD_POPUP_NUMBER);

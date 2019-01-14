a709c78bd3587901509e3c81471ccfa3553ccd81
Shmitkin <mitkinoleg@gmail.com>
2019-01-14 21:11:12 +0300

    Сделал получение данных с сервера

--- js/pin.js
+++ js/pin.js
@@ -2,32 +2,32 @@
 
 (function () {
 
-  var CARD_COUNT = 8;
-  var createPin = function (cardPin) {
+  var createPin = function (card) {
     var mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
     var anotherPin = mapPin.cloneNode(true);
     var mapPinImg = anotherPin.querySelector('img');
-    anotherPin.style.left = cardPin.location.x - (window.utils.MAP_PIN_WIDTH / 2) + 'px';
-    anotherPin.style.top = cardPin.location.y - window.utils.MAP_PIN_HEIGHT + 'px';
-    mapPinImg.src = cardPin.avatar;
-    mapPinImg.alt = cardPin.offer.title;
+    anotherPin.style.left = card.location.x - (window.utils.MAP_PIN_WIDTH / 2) + 'px';
+    anotherPin.style.top = card.location.y - window.utils.MAP_PIN_HEIGHT + 'px';
+    mapPinImg.src = card.author.avatar;
+    mapPinImg.alt = card.offer.title;
     anotherPin.addEventListener('click', function () {
       window.utils.removeMapCard();
-      window.popup.fillPopUpInfo(cardPin);
+      window.popup.fillPopUpInfo(card);
     });
     return anotherPin;
   };
 
   var renderPins = function () {
-    var map = document.querySelector('.map');
-    var fragment = document.createDocumentFragment();
-    var cardsList = window.card.cardsList(CARD_COUNT);
-    window.form.enableForm();
-    window.utils.removePins();
-    for (var i = 0; i < cardsList.length; i++) {
-      fragment.appendChild(createPin(cardsList[i]));
-    }
-    map.appendChild(fragment);
+    window.loadCardsInfo(function (cards) {
+      var map = document.querySelector('.map');
+      var fragment = document.createDocumentFragment();
+      window.form.enableForm();
+      window.utils.removePins();
+      for (var i = 0; i < cards.length; i++) {
+        fragment.appendChild(createPin(cards[i]));
+      }
+      map.appendChild(fragment);
+    });
   };
 
 

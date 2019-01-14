1f6d78e576676a667e436f4b43d736ad746bb6aa
Shmitkin <mitkinoleg@gmail.com>
2019-01-14 16:56:22 +0300

    Разбил программу на модули.

---
+++ js/main-pin-handle.js
@@ -0,0 +1,73 @@
+'use strict';
+(function () {
+  var mainPinHandle = document.querySelector('.map__pin--main');
+  var map = document.querySelector('.map');
+  var LIMITS_MAIN_PIN = {
+    top: 130 - window.utils.MAP_PIN_HEIGHT,
+    left: 0,
+    right: map.clientWidth - window.utils.MAP_PIN_WIDTH,
+    bottom: 630
+  };
+
+  mainPinHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPinHandle.style.top = (mainPinHandle.offsetTop - shift.y) + 'px';
+      mainPinHandle.style.left = (mainPinHandle.offsetLeft - shift.x) + 'px';
+
+      if (mainPinHandle.offsetTop - shift.y > LIMITS_MAIN_PIN.bottom) {
+        mainPinHandle.style.top = LIMITS_MAIN_PIN.bottom + 'px';
+      } else if (mainPinHandle.offsetTop - shift.y < LIMITS_MAIN_PIN.top) {
+        mainPinHandle.style.top = LIMITS_MAIN_PIN.top + 'px';
+      }
+      if (mainPinHandle.offsetLeft - shift.x < LIMITS_MAIN_PIN.left) {
+        mainPinHandle.style.left = LIMITS_MAIN_PIN.left + 'px';
+      } else if (mainPinHandle.offsetLeft - shift.x > LIMITS_MAIN_PIN.right) {
+        mainPinHandle.style.left = LIMITS_MAIN_PIN.right + 'px';
+      }
+
+      fillAddressInput(mainPinHandle.style.left, mainPinHandle.style.top);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      checkPins();
+    };
+
+    var fillAddressInput = function (clientX, clientY) {
+      var x = clientX.split('px').join('') - window.utils.MAP_PIN_WIDTH / 2;
+      var y = clientY.split('px').join('') - window.utils.MAP_PIN_HEIGHT;
+      var address = document.querySelector('#address');
+      address.value = x + ', ' + y;
+    };
+
+    var checkPins = function () {
+      var mapPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+      if (mapPins.length <= 0) {
+        window.pin.renderPins();
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+})();
---
+++ js/pin.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+  var MAIN_PIN_X = 570;
+  var MAIN_PIN_Y = 375;
+  var CARD_COUNT = 8;
+  var createPin = function (cardPin) {
+    var mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
+    var anotherPin = mapPin.cloneNode(true);
+    var mapPinImg = anotherPin.querySelector('img');
+    anotherPin.style.left = cardPin.location.x - (window.utils.MAP_PIN_WIDTH / 2) + 'px';
+    anotherPin.style.top = cardPin.location.y - window.utils.MAP_PIN_HEIGHT + 'px';
+    mapPinImg.src = cardPin.avatar;
+    mapPinImg.alt = cardPin.offer.title;
+    anotherPin.addEventListener('click', function () {
+      window.utils.removeMapCard();
+      window.popup.fillPopUpInfo(cardPin);
+    });
+    return anotherPin;
+  };
+
+  var renderPins = function () {
+    var map = document.querySelector('.map');
+    var fragment = document.createDocumentFragment();
+    var cardsList = window.card.cardsList(CARD_COUNT);
+    window.form.enableForm();
+    window.utils.removePins();
+    for (var i = 0; i < cardsList.length; i++) {
+      fragment.appendChild(createPin(cardsList[i]));
+    }
+    map.appendChild(fragment);
+  };
+
+
+  // Exports
+  window.pin = {
+    renderPins: renderPins
+  };
+
+})();
